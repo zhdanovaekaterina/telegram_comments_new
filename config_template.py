8b2ede0f9ec2e94e0e9477bce4154757d07ba6da
e.nasedkina <zhdanova.ekaterina.95@yandex.ru>
2022-08-10 08:26:43 +0300

    Some small updates

--- config_template.py
+++ config_template.py
@@ -1,7 +1,12 @@
+# Telegram access
+api_id = ''
+api_hash = ''
 bot_token = ''
 
+# Password for posts deleting
+password = ''
+
+# File names
 workbase_name = 'workbase.db'
 post_info_temp_name = 'post_info_temp.csv'
 post_id_temp_name = 'post_id_temp.csv'
-
-password = ''
